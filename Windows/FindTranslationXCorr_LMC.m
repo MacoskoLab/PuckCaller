94506cb3ad4fbd75bbf52f93b5df5a343d9cf821
MacoskoLab <evan_macosko@hms.harvard.edu>
2020-02-20 17:41:40 -0500

    Add files via upload

---
+++ Windows/FindTranslationXCorr_LMC.m
@@ -0,0 +1,30 @@
+function ImageOut=FindTranslationXCorr_LMC(ImageIn,varargin)
+%We are given a stack of images
+%We align them using xcorr.
+%We then output the aligned stack.
+%Input and output should both be uint16.
+ImageOut=zeros(size(ImageIn));
+ImageOut(:,:,1)=ImageIn(:,:,1);
+
+b=[1,size(ImageIn,1),1,size(ImageIn,2)];
+index = find(cellfun(@(x) (all(ischar(x)) || isstring(x))&&(string(x)=="XCorrBounds"), varargin, 'UniformOutput', 1));
+if ~isempty(index)
+    b=varargin{index+1};
+end
+
+   
+ imBinMap(:,:)=ImageIn(b(1):b(2),b(3):b(4),1)>median(median(ImageIn(:,:,1)));
+for j=2:size(ImageIn,3)
+    imBinChannel=ImageIn(b(1):b(2),b(3):b(4),j)>median(median(ImageIn(:,:,j)));
+    Cor=xcorr2(uint8(imBinMap),uint8(imBinChannel));
+    [ssr,snd]=max(Cor(:));
+    [y,x]=ind2sub(size(Cor),snd);
+    %[y,x]=ind2sub(size(Cor),find(Cor(:)==max(Cor(:))));
+    offset=[-((b(4)-b(3)+1-x)),-(b(2)-b(1)+1-y)]
+%    figure(8)
+%    imshowpair(ImageIn(b(1):b(2),b(3):b(4),1)>300,ImageIn(b(1):b(2),b(3):b(4),j)>300)
+%    figure(9)
+%    imshowpair(ImageIn(b(1):b(2),b(3):b(4),1)>300,imtranslate(ImageIn(b(1):b(2),b(3):b(4),j)>300,offset))
+    ImageOut(:,:,j)=uint16(imtranslate(ImageIn(:,:,j),offset));    
+end
+end
